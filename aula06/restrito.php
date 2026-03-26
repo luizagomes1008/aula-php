9631e321cdbc88b486782a93e4175427e254b8a8
Lu♡ <luizagomes10082009@gmail.com>
2026-03-26 12:21:03 -0300

    Add files via upload

---
+++ aula06/restrito.php
@@ -0,0 +1,17 @@
+<?php
+    $nome= $_GET['nome']
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>restrito</title>
+    <h1> restritooooooooooooooo</h1>
+    <p>bem vindo <?php echo $nome; ?> a sua area restritas</p>
+</head>
+<body>
+    
+</body>
+</html>
