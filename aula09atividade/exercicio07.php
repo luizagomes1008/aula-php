6886dd79e5ffaabc21243041fc955fbb7dc89924
Lu♡ <luizagomes10082009@gmail.com>
2026-05-28 12:22:29 -0300

    Add files via upload

--- aula09atividade/exercicio07.php
+++ aula09atividade/exercicio07.php
@@ -1,11 +1,13 @@
 <?php
     $numeros = [14, 23, 2, 1, 80, 4 ];
+    echo"numeros na ordem crescente";
     sort($numeros);
     foreach ($numeros as $indice) { 
         echo "$indice";
     }
     echo"<hr>";
 
+    echo"numeros na ordem decrescente";
     rsort($numeros);
     foreach ($numeros as $indice) { 
         echo "$indice";
--- aula09atividade/exercicio09.php
+++ aula09atividade/exercicio09.php
@@ -1,9 +1,20 @@
 <?php
     $numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    echo"vetor geral: ";
+     foreach ($numeros as $key) {
+        echo"$key";
+    }
+
+    echo"<hr>";
+    echo"vetor par: ";
+    $numerospare = [];
     foreach ($numeros as $key) {
         if ($key%2==0) {
-            echo"$key";
+            $numerospare[] = "$key";
         }
+    }
+    foreach ($numerospare as $key) {
+        echo"$key";
     }
 
 
